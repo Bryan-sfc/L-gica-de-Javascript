87098ac6b3da0cf95bba429d9ea4db4902964ec7
Bryan Inacio <bryan.inacio@aluno.senai.br>
2025-04-15 16:43:29 -0300

    feat: Commit inicial

---
+++ Desafios/Ex01.js
@@ -0,0 +1,16 @@
+//<1. Formatação de CPF>
+//Crie uma função que recebe um número como string (ex: &#39;12345678901&#39;) 
+// e retorna no formato de CPF (123.456.789-01) usando slice e concatenação.
+
+var prompt = require('prompt-sync')();
+
+var CPF;
+
+CPF = prompt("Digite seu CPF, 11 dígitos: ")
+
+console.log(`CPF: ${formataCPF(CPF)}`);
+
+function formataCPF(CPF){
+  //realizar a formatação...
+    return CPF.slice(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
---
+++ Desafios/Ex02.js
@@ -0,0 +1,18 @@
+//<2. Maior Palavra>
+//Peça uma frase e crie uma função que retorna a maior palavra da frase usando
+//split() e reduce().
+
+var prompt = require('prompt-sync')();
+
+let frase = [];
+
+frase = prompt("Escreva uma frase: ");
+
+var espacos = frase.split(' ');
+
+function maiorPalavra(palavra) {
+    espacos.lenght == 
+}
+
+console.log(maiorPalavra);
+
