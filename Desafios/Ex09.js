64b05715fdb39ef89e4b66bfa2325be794c5cd2e
Bryan <goncalvesbryan424@gmail.com>
2025-04-18 13:10:31 -0300

    feat: Praticando JS

--- Desafios/Ex09.js
+++ Desafios/Ex09.js
@@ -1,3 +1,21 @@
 console.log("<9. Verificar Palíndromo com Funções>");
 // Crie uma função que verifica se uma palavra é palíndroma usando split(), reverse()
 // e join().
+
+var prompt = require('prompt-sync')();
+
+function palindromo(){
+    const original = frase.toLowerCase();
+    
+    const invertida = original.split('').reverse('').join('');
+    
+    return original === invertida;
+}
+
+let frase = prompt("Escreva uma frase qualquer: ");
+
+if (palindromo(frase)) {
+    console.log("É um palíndromo!");
+} else {
+    console.log("Não é um palíndromo.");
+}
